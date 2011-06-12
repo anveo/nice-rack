d94af6b2098392b2142b56aae6c6cd83a42ce37f
Brian Racer <brian@jetpackweb.com>
2011-06-12 15:11:52 -0600

    path fix

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,9 +11,9 @@ timeout 30
 
 # This is where we specify the socket.
 # We will point the upstream Nginx module to this socket later on
-listen "/var/www/nice-rack/tmp/sockets/unicorn.sock", :backlog => 64
+listen "/var/www/nice-rack/shared/tmp/sockets/unicorn.sock", :backlog => 64
 
-pid "/var/www/nice-rack/tmp/pids/unicorn.pid"
+pid "/var/www/nice-rack/shared/tmp/pids/unicorn.pid"
 
 # Set the path of the log files inside the log folder of the testapp
 stderr_path "/var/www/nice-rack/log/unicorn.stderr.log"
