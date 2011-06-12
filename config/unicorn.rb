4e7ddde91c37630f6c55d893889a89c7be9f8225
Brian Racer <brian@jetpackweb.com>
2011-06-12 15:17:18 -0600

    path fixes

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,13 +11,13 @@ timeout 30
 
 # This is where we specify the socket.
 # We will point the upstream Nginx module to this socket later on
-listen "/var/www/nice-rack/shared/tmp/sockets/unicorn.sock", :backlog => 64
+listen "/var/www/nice-rack/shared/sockets/unicorn.sock", :backlog => 64
 
-pid "/var/www/nice-rack/shared/tmp/pids/unicorn.pid"
+pid "/var/www/nice-rack/shared/pids/unicorn.pid"
 
 # Set the path of the log files inside the log folder of the testapp
-stderr_path "/var/www/nice-rack/log/unicorn.stderr.log"
-stdout_path "/var/www/nice-rack/log/unicorn.stdout.log"
+stderr_path "/var/www/nice-rack/shared/log/unicorn.stderr.log"
+stdout_path "/var/www/nice-rack/shared/log/unicorn.stdout.log"
 
 before_fork do |server, worker|
 # This option works in together with preload_app true setting
